f261f7c4fcc8cfb914a28eb62b3898fd70ae9840
lucalarsen <lucalarsen@hotmail.com>
2019-03-27 13:28:38 +0100

    har lavet en post med TS

--- src/js/index.ts
+++ src/js/index.ts
@@ -9,6 +9,32 @@ let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("c
 let GetAllCarsbtn : HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllbtn");
 GetAllCarsbtn.addEventListener("click", ShowAllCars);
 
+let AddCarbtn : HTMLButtonElement = <HTMLButtonElement> document.getElementById("addCarbtn");
+AddCarbtn.addEventListener("click", AddCar );
+
+
+function AddCar() :void{
+    let addmodelelement: HTMLInputElement = <HTMLInputElement> document.getElementById("inputModel")
+    let addvendorelement: HTMLInputElement = <HTMLInputElement> document.getElementById("inputVendor")
+    let addmpriceelement: HTMLInputElement = <HTMLInputElement> document.getElementById("inputPrice")
+
+    let mymodel : string = addmodelelement.value;
+    let myvendor : string = addvendorelement.value;
+    let myprice : number = +addmpriceelement.value;
+
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars", {
+        model: mymodel,
+        vendor: myvendor,
+        price: myprice
+      })
+      .then(function (response : AxiosResponse) : void {
+        console.log("Statuskoden er :"+response.status);
+      })
+      .catch(function (error : AxiosError): void {
+        console.log(error);
+      });
+}
+
 function ShowAllCars() : void{
     axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
 
@@ -20,7 +46,7 @@ function ShowAllCars() : void{
 
         response.data.forEach((car: ICar) => {
             //result += "<li>"+car.model + ""+car.vendor+""+car.id+"</li>"
-           result += `<li> bil ${car.model} sælger ${car.vendor} bil id ${car.id} bilen koster ${car.price} kr.</li>`
+           result += `<li> bil model ${car.model} sælger ${car.vendor} bil id ${car.id} bilen koster ${car.price} kr.</li>`
             
         });
         result += "</ol>"
