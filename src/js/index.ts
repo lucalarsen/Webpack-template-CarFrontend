b283423242c38cebb6b61aa49ce276cf6131a6c0
lucalarsen <lucalarsen@hotmail.com>
2019-03-27 12:26:00 +0100

    then og catch færdig

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,7 +2,28 @@ import axios, {
     AxiosResponse,
     AxiosError
 } from "../../node_modules/axios/index"
+import { ICar } from "./ICar";
 
 
 let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 
+function ShowAllCars() : void{
+    axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+
+    .then(function (response : AxiosResponse<ICar[]>): void
+    {
+        console.log(response);
+  
+    })
+
+    .catch(function(error: AxiosError): void
+    {
+        console.log("ERROR");
+        console.log(error);
+    })
+    console.log("er i slutningen af ShowAllCars function");
+}
+
+
+
+ShowAllCars();
