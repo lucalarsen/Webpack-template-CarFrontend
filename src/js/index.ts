35911c0219fbebedab104a4d5e603b6bdeb4ae15
lucalarsen <lucalarsen@hotmail.com>
2019-03-27 11:19:13 +0100

    har lavet then og catch

---
+++ src/js/index.ts
@@ -0,0 +1,8 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+
+let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
